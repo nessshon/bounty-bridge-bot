feae414ca440c87523db8ff3b2a56afc78528e39
nessshon <shonness98@gmail.com>
2024-02-14 19:01:16 +0500

    Add sulguk for HTML converter to telegram entities.

--- project/bot/middlewares/__init__.py
+++ project/bot/middlewares/__init__.py
@@ -1,14 +1,17 @@
-from aiogram import Dispatcher
+from aiogram import Dispatcher, Bot
+from sulguk import AiogramSulgukMiddleware
 
 from .database import DBSessionMiddleware
 from .manager import ManagerMiddleware
 from .throttling import ThrottlingMiddleware
 
 
-def bot_middlewares_register(dp: Dispatcher, **kwargs) -> None:
+def bot_middlewares_register(dp: Dispatcher, bot: Bot, **kwargs) -> None:
     """
     Register bot middlewares.
     """
+    bot.session.middleware(AiogramSulgukMiddleware())
+
     dp.update.outer_middleware.register(DBSessionMiddleware(kwargs["sessionmaker"]))
     dp.update.outer_middleware.register(ThrottlingMiddleware())
     dp.update.outer_middleware.register(ManagerMiddleware())
